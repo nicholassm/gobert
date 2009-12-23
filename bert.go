a7e8921daeba2e3a1685493d104425fc065db2ee
Andreas Garnæs <andreas.garnaes@gmail.com>
2009-12-23 14:24:23 +0100

    More types, more tests, slight refactoring.

--- bert.go
+++ bert.go
@@ -3,6 +3,7 @@ package bert
 import (
   "fmt";
 	"io";
+  "math";
 	"os";
 	"reflect";
 	"strings";
@@ -23,8 +24,6 @@ const (
   SMALL_BIGNUM = 110;
   LARGE_BIGNUM = 111;
   MAGIC        = 131;
-  MAX_INT      = (1 << 27) -1;
-  MIN_INT      = -(1 << 27);
 )
 
 func writeByte(w io.Writer, data byte) os.Error {
@@ -32,7 +31,6 @@ func writeByte(w io.Writer, data byte) os.Error {
   return err;
 }
 
-// Write string as atom to w.
 func writeAtom(w io.Writer, atom string) os.Error {
   writeByte(w, ATOM);
   writeByte(w, uint8(len(atom)));
@@ -58,9 +56,120 @@ func writeTuple(w io.Writer, tuple []interface{}) os.Error {
 	return nil;
 }
 
+func writeNil(w io.Writer) os.Error {
+  w.Write([]byte {SMALL_TUPLE, 2});
+  writeAtom(w, "bert");
+  writeAtom(w, "nil");
+  
+  return nil
+}
+
+func writeBool(w io.Writer, v bool) os.Error {
+  w.Write([]byte {SMALL_TUPLE, 2});
+  writeAtom(w, "bert");
+  
+  if v {
+    writeAtom(w, "true")
+  } else {
+    writeAtom(w, "false")
+  }
+  
+  return nil
+}
+
+
+func writeUint8(w io.Writer, v uint8) (err os.Error) {
+  _, err = w.Write([]byte {SMALL_INT, v});
+  return
+}
+
+func writeUint16(w io.Writer, v uint16) (err os.Error) {
+  _, err = w.Write([] byte { SMALL_INT,
+    byte(v >> 8),
+    byte(v)
+  });
+  return
+}
+
+func writeUint32(w io.Writer, v uint32) (err os.Error) {
+  _, err = w.Write([] byte {
+    SMALL_INT,
+    byte(v >> 24),
+    byte(v >> 16),
+    byte(v >> 8),
+    byte(v)
+  });
+  return
+}
+
+func writeUint64(w io.Writer, v uint64) (err os.Error) {
+  _, err = w.Write([] byte {
+    SMALL_BIGNUM,
+    8,
+    0,
+    byte(v >> 56),
+    byte(v >> 48),
+    byte(v >> 40),
+    byte(v >> 32),
+    byte(v >> 24),
+    byte(v >> 16),
+    byte(v >> 8),
+    byte(v)
+  });
+  return
+}
+
+func writeInt64(w io.Writer, v int64) (err os.Error) {
+  var sign byte;
+  
+  if v > 0 {
+    sign = 0
+  } else {
+    sign = 1;
+    v    = -v
+  };
+  
+  _, err = w.Write([] byte {
+    SMALL_BIGNUM,
+    8,
+    sign,
+    byte(v),
+    byte(v >> 8),
+    byte(v >> 16),
+    byte(v >> 24),
+    byte(v >> 32),
+    byte(v >> 40),
+    byte(v >> 48),
+    byte(v >> 56)
+  });
+  return
+}
+
+func writeBinary(w io.Writer, data []byte) os.Error {
+  l := len(data);
+  
+  if l > math.MaxInt32 {
+    return os.NewError(fmt.Sprintf("bert.Encode: binary too large: %d (max is %d)", l, math.MaxInt32));
+  }
+	
+  writeByte(w, BIN);
+  binary.Write(w, binary.BigEndian, uint32(l));
+  w.Write(data);
+  
+  return nil
+}
+
+func writeFloat(w io.Writer, v string) os.Error {
+  writeByte(w, FLOAT);
+  fmt.Fprintf(w, v);
+  
+  return nil
+}
+
 func writeArrayOrSlice(w io.Writer, val reflect.ArrayOrSliceValue) os.Error {
   writeByte(w, LIST);
-
+  binary.Write(w, binary.BigEndian, uint32(val.Len()));
+  
 	for i := 0; i < val.Len(); i++ {
 		if err := writeValue(w, val.Elem(i)); err != nil {
 			return err
@@ -78,10 +187,14 @@ func writeMap(w io.Writer, val *reflect.MapValue) os.Error {
   writeAtom(w, "dict");
   
 	keys := val.Keys();
+	writeByte(w, LIST);
+  binary.Write(w, binary.BigEndian, uint32(len(keys)));
 	for i := 0; i < len(keys); i++ {
+	  w.Write([]byte {SMALL_TUPLE, 2});
 		writeValue(w, keys[i]);
 		writeValue(w, val.Elem(keys[i]));
 	}
+	writeByte(w, NIL);
 
 	return nil;
 }
@@ -92,50 +205,23 @@ func writeMap(w io.Writer, val *reflect.MapValue) os.Error {
 // Thus in the big switch, matching on reflect.IntValue is no good. Workaround?
 func writeValue(w io.Writer, val reflect.Value) (err os.Error) {
 	switch v := val.(type) {
-	case *reflect.Int8Value:
-	  // SMALL_INT
-	  writeByte(w, SMALL_INT);
-	  writeByte(w, uint8(v.Get()));
-	case *reflect.Int32Value:
-	  // INT
-	  writeByte(w, INT);
-	  binary.Write(w, binary.BigEndian, v.Get());
-	case nil:
-	  // NIL
-    writeByte(w, SMALL_TUPLE);
-    writeByte(w, 2);
-	  writeAtom(w, "bert");
-	  writeAtom(w, "nil")
-	case *reflect.FloatValue:
-	  // FLOAT
-	  writeByte(w, FLOAT);
-	  fmt.Fprintf(w, "%.20e", v.Get());
-  case *reflect.StringValue:
-	  // STRING
-	  bytes := strings.Bytes(v.Get());
-	  writeByte(w, BIN);
-	  writeByte(w, uint8(len(bytes)));
-	  w.Write(bytes);
-	case *reflect.ArrayValue:
-	  // LIST
-	case *reflect.Int64Value:
-	  // SMALL_BIGNUM
-	case *reflect.MapValue:
-	  // {bert, dict, keysAndValues}
-    writeByte(w, SMALL_TUPLE);
-    writeByte(w, 3);
-	  writeAtom(w, "bert");
-	  writeMap(w, v);
-	case *reflect.BoolValue:
-	  // {bert, true} or {bert, false}
-    writeByte(w, SMALL_TUPLE);
-    writeByte(w, 2);
-	  writeAtom(w, "bert");
-	  if v.Get() {
-	    writeAtom(w, "true");
-	  } else {
-	    writeAtom(w, "false");
-	  }
+	case nil:                   return writeNil(w)
+	case *reflect.BoolValue:    return writeBool(w, v.Get())
+	case *reflect.Uint8Value:	  return writeUint8(w, v.Get())
+	case *reflect.Int8Value:	  return writeUint8(w, uint8(v.Get()))
+	case *reflect.Uint16Value:  return writeUint16(w, v.Get())
+	case *reflect.Int16Value:	  return writeUint16(w, uint16(v.Get()))
+	case *reflect.Uint32Value:  return writeUint32(w, v.Get())
+	case *reflect.Int32Value:	  return writeUint32(w, uint32(v.Get()))
+	case *reflect.Uint64Value:  return writeUint64(w, v.Get())
+	case *reflect.Int64Value:	  return writeInt64(w, v.Get())
+	case *reflect.FloatValue:   return writeFloat(w, fmt.Sprintf("%.20e", v.Get()))
+	case *reflect.Float32Value: return writeFloat(w, fmt.Sprintf("%.20e", v.Get()))
+	case *reflect.Float64Value: return writeFloat(w, fmt.Sprintf("%.20e", v.Get()))
+  case *reflect.StringValue:  return writeBinary(w, strings.Bytes(v.Get()))
+	case *reflect.ArrayValue:   return writeArrayOrSlice(w, v)
+	case *reflect.SliceValue:   return writeArrayOrSlice(w, v)
+	case *reflect.MapValue:     return writeMap(w, v)
 	default:
 		return os.NewError("bert.Encode: invalid type " + v.Type().String())
 	}
@@ -144,6 +230,7 @@ func writeValue(w io.Writer, val reflect.Value) (err os.Error) {
 }
 
 func Encode(w io.Writer, val interface{}) os.Error {
+  writeByte(w, MAGIC);
 	return writeValue(w, reflect.NewValue(val))
 }
 
--- bert_test.go
+++ bert_test.go
@@ -20,21 +20,46 @@ func testEncode(t *testing.T, message string, val interface{}, expected []byte) 
 }
 
 func TestEncodeNil(t *testing.T) {
-  expected := []byte {SMALL_TUPLE, 2, ATOM, 4, 'b', 'e', 'r', 't', ATOM, 3, 'n', 'i', 'l'};
+  expected := []byte {MAGIC, SMALL_TUPLE, 2, ATOM, 4, 'b', 'e', 'r', 't', ATOM, 3, 'n', 'i', 'l'};
   testEncode(t, "Encoding nil failed", nil, expected);
 }
 
 func TestEncodeTrue(t *testing.T) {
-  expected := []byte {SMALL_TUPLE, 2, ATOM, 4, 'b', 'e', 'r', 't', ATOM, 4, 't', 'r', 'u', 'e'};
+  expected := []byte {MAGIC, SMALL_TUPLE, 2, ATOM, 4, 'b', 'e', 'r', 't', ATOM, 4, 't', 'r', 'u', 'e'};
   testEncode(t, "Encoding true failed", true, expected);
 }
 
 func TestEncodeFalse(t *testing.T) {
-  expected := []byte {SMALL_TUPLE, 2, ATOM, 4, 'b', 'e', 'r', 't', ATOM, 5, 'f', 'a', 'l', 's', 'e'};
+  expected := []byte {MAGIC, SMALL_TUPLE, 2, ATOM, 4, 'b', 'e', 'r', 't', ATOM, 5, 'f', 'a', 'l', 's', 'e'};
   testEncode(t, "Encoding true failed", false, expected);
 }
 
 func TestEncodeFloat(t *testing.T) {
-  expected := bytes.Add([]byte {FLOAT}, strings.Bytes(fmt.Sprintf("%.20e", 1.2e3)));
+  expected := bytes.Add([]byte {MAGIC, FLOAT}, strings.Bytes(fmt.Sprintf("%.20e", float(1.2e3))));
   testEncode(t, "Encoding float failed", 1.2e3, expected);
+}
+
+func TestEncodeFloat32(t *testing.T) {
+  expected := bytes.Add([]byte {MAGIC, FLOAT}, strings.Bytes(fmt.Sprintf("%.20e", float32(1.2e3))));
+  testEncode(t, "Encoding float32 failed", 1.2e3, expected);
+}
+
+func TestEncodeFloat64(t *testing.T) {
+  expected := bytes.Add([]byte {MAGIC, FLOAT}, strings.Bytes(fmt.Sprintf("%.20e", float64(1.2e3))));
+  testEncode(t, "Encoding float64 failed", 1.2e3, expected);
+}
+
+func TestEncodeString(t *testing.T) {
+  expected := bytes.Add([]byte {MAGIC, BIN, 0, 0, 0, 3}, strings.Bytes("foo"));
+  testEncode(t, "Encoding string failed", "foo", expected);
+}
+
+func TestEncodeArray(t *testing.T) {
+  expected := []byte {MAGIC, LIST, 0, 0, 0, 2, SMALL_INT, 1, SMALL_INT, 2, NIL};
+  testEncode(t, "Encoding array failed", []byte {1, 2}, expected);
+}
+
+func TestEncodeMap(t *testing.T) {
+  expected := []byte {MAGIC, SMALL_TUPLE, 3, ATOM, 4, 'b', 'e', 'r', 't', ATOM, 4, 'd', 'i', 'c', 't', LIST, 0, 0, 0, 1, SMALL_TUPLE, 2, SMALL_INT, 1, SMALL_INT, 2, NIL};
+  testEncode(t, "Encoding map failed", map[byte]byte {1: 2}, expected);
 }
